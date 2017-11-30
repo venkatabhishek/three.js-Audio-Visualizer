490f96ec7d4223b6fa2b4749f7619893340e2046
Glitch (hello-website) <none>
2017-11-30 00:13:57 +0000

    👢🏩 Checkpoint index.html:571258 script.js:571258 styles.css:571258

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log('hi');
+window.onload = function() {
+  var ctx = new AudioContext();
+  var audio = document.getElementById('myAudio');
+  var audioSrc = ctx.createMediaElementSource(audio);
+  var analyser = ctx.createAnalyser();
+  // we have to connect the MediaElementSource with the analyser 
+  audioSrc.connect(analyser);
+  // we could configure the analyser: e.g. analyser.fftSize (for further infos read the spec)
+ 
+  // frequencyBinCount tells you how many values you'll receive from the analyser
+  var frequencyData = Uint8Array(analyser.frequencyBinCount);
+ 
+  // we're ready to receive some data!
+  // loop
+  function renderFrame() {
+     requestAnimationFrame(renderFrame);
+     // update data in frequencyData
+     analyser.getByteFrequencyData(frequencyData);
+     // render frame based on values in frequencyData
+     //console.log(frequencyData)
+  }
+  audio.start();
+  renderFrame();
+};
