9bc558e91308278e983b2501694389ce8d97c315
Glitch (hello-website) <none>
2017-12-02 21:30:30 +0000

    ☃️👂 Checkpoint script.js:571258 styles.css:571258

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ window.onload = function() {
 
     var camera = new THREE.PerspectiveCamera(10, window.innerWidth / window.innerHeight, 0.1, 80000);
     camera.position.set(0, 10, 1000);
-  
+
     camera.lookAt(scene.position);
 
     var renderer = new THREE.WebGLRenderer({
@@ -25,25 +25,98 @@ window.onload = function() {
 
     document.body.appendChild(renderer.domElement);
 
-  //draw lines from frequency magnitudes
+    //draw lines from frequency magnitudes
+    function getColoredBufferLine(steps, phase, geometry) {
+
+        var vertices = geometry.vertices;
+        var segments = geometry.vertices.length;
+
+        // geometry
+        var geometry = new THREE.BufferGeometry();
+
+        // material
+        var lineMaterial = new THREE.LineBasicMaterial({
+            vertexColors: THREE.VertexColors
+        });
+
+        // attributes
+        var positions = new Float32Array(segments * 3); // 3 vertices per point
+        var colors = new Float32Array(segments * 3);
+
+        var frequency = 1 / (steps * segments);
+        var color = new THREE.Color();
+
+        var x, y, z;
+
+        for (var i = 0, l = segments; i < l; i++) {
+
+            x = vertices[i].x;
+            y = vertices[i].y;
+            z = vertices[i].z;
+
+            positions[i * 3] = x;
+            positions[i * 3 + 1] = y;
+            positions[i * 3 + 2] = z;
+
+            color.set(makeColorGradient(i, frequency, phase));
+
+            colors[i * 3] = color.r;
+            colors[i * 3 + 1] = color.g;
+            colors[i * 3 + 2] = color.b;
+
+        }
+
+        geometry.addAttribute('position', new THREE.BufferAttribute(positions, 3));
+        geometry.addAttribute('color', new THREE.BufferAttribute(colors, 3));
+
+        // line
+        var line = new THREE.Line(geometry, lineMaterial);
+
+        return line;
+
+    }
+
+    function makeColorGradient(i, frequency, phase) {
+
+        var center = 128;
+        var width = 127;
+
+        var redFrequency, grnFrequency, bluFrequency;
+        grnFrequency = bluFrequency = redFrequency = frequency;
+
+        var phase2 = phase + 2;
+        var phase3 = phase + 4;
+
+        var red = Math.sin(redFrequency * i + phase) * width + center;
+        var green = Math.sin(grnFrequency * i + phase2) * width + center;
+        var blue = Math.sin(bluFrequency * i + phase3) * width + center;
+
+        return parseInt('0x' + _byte2Hex(red) + _byte2Hex(green) + _byte2Hex(blue));
+    }
+
+    function _byte2Hex(n) {
+        var nybHexString = "0123456789ABCDEF";
+        return String(nybHexString.substr((n >> 4) & 0x0F, 1)) + nybHexString.substr(n & 0x0F, 1);
+    }
+
     function geo(arr) {
-      var material = new THREE.LineBasicMaterial({
-	color: 0x0000ff
-});
+
         var geometry = new THREE.Geometry();
-      
-      for(var i = 0; i < arr.length;i++){
-       var r =  arr[i] + 10;
-        var theta = (2*Math.PI/1024)*i*4;
-      
-geometry.vertices.push(
-	new THREE.Vector3( r*Math.cos(theta), r*Math.sin(theta),4)
-);
-        
-      }
-      var line = new THREE.Line( geometry, material );
-scene.add( line );
- 
+
+        for (var i = 0; i < arr.length; i++) {
+            var r = arr[i] + 10;
+            var theta = (2 * Math.PI / 1024) * i * 4;
+
+            geometry.vertices.push(
+                new THREE.Vector3(r * Math.cos(theta), r * Math.sin(theta), 4)
+            );
+
+        }
+
+        var steps = 0.09;
+        var phase = 1.9;
+        var coloredLine = getColoredBufferLine(steps, phase, geometry);
+        scene.add(coloredLine);
 
     }
 
@@ -66,7 +139,7 @@ scene.add( line );
 
     // frequencyBinCount tells you how many values you'll receive from the analyser
     var frequencyData = new Uint8Array(analyser.frequencyBinCount);
-    
+
     // we're ready to receive some data!
     // loop
 
