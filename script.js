e318395ad5ace72c08a6cad948da65296fd44df4
Glitch (hello-website) <none>
2017-12-03 04:49:31 +0000

    🐦😎 Checkpoint script.js:571258 styles.css:571258

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ window.onload = function() {
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(window.innerWidth, window.innerHeight);
     
-    $(document).append(renderer.domElement);
+    document.body.appendChild(renderer.domElement);
     
 
     //draw lines from frequency magnitudes
